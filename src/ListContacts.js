70ffc00dd60a7e59c61941a5709bb65c176cac71
Josephine Engreitz <jo.engreitz@gmail.com>
2018-03-28 17:52:53 -0400

    Add 'now showing' section with link to clear the search query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,10 @@ updateQuery = (query) => {
   this.setState({ query: query.trim() })
 }
 
+clearQuery = () => {
+  this.setState({ query: ''})
+}
+
   render () {
     const {contacts, onDeleteContact} = this.props
     const {query} = this.state
@@ -42,6 +46,13 @@ updateQuery = (query) => {
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length}} total</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
 
          <ol className='contact-lists'>
           {showingContacts.map((contact) => (
