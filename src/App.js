ac8534db82cb69e46c80eb5d9e8906d4a2a120c1
Josephine Engreitz <jo.engreitz@gmail.com>
2018-03-29 15:56:15 -0400

    Save new contact to the server

--- src/App.js
+++ src/App.js
@@ -21,16 +21,32 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
   }
+
+createContact(contact) {
+  ContactsAPI.create(contact).then(contact => {
+    this.setState(state => {
+      contacts: state.contacts.concat([contact])
+    })
+  })
+}
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => (
+        <Route exact path='/' render={() => (
           <ListContacts
             onDeleteContact={this.removeContact}
             contacts={this.state.contacts}
           />
         )}/>
-        <Route path="/create" component={CreateContact}/>
+        <Route path='/create' render={(history) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
     )
   }
