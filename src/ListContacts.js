a7d0a51dc2b12b8f7fad666a9b2509f27883386a
Jo Engreitz <jo.engreitz@gmail.com>
2018-03-31 14:30:09 -0400

    fix: Fixed contact avatar typo in ListContacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -63,7 +63,7 @@ clearQuery = () => {
           {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div className='contact-avatar' style={{
-                backgroundImage: 'url(s{contact.avatarURL})'
+                backgroundImage: `url(${contact.avatarURL})`
               }}/>
               <div className='contact-details'>
                 <p>{contact.name}</p>
